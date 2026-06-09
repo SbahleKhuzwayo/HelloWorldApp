254d8ea489c2bd3cced3b7050180e18cb126c493
Sbahle Khuzwayo <sbahlekhuz2000@gmail.com>
2026-06-09 13:53:07 +0200

    Initial commit - Copilot Hello World app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Random random = new Random();
+
+for (int i = 1; i <= 5; i++)
+{
+    int a = random.Next(1, 100);
+    int b = random.Next(1, 100);
+    
+    Console.WriteLine($"Equation {i}:");
+    Console.WriteLine($"  Addition: {a} + {b} = {a + b}");
+    Console.WriteLine($"  Subtraction: {a} - {b} = {a - b}");
+    Console.WriteLine();
+}
